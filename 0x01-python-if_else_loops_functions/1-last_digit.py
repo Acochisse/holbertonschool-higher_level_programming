1c3f6868b0c6286760aea22816f19e4568d4e96e
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-01 12:34:48 -0500

    created and tested 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+x = number
+print("Last digit of", x, "is", end= " ")
+if x > 0:
+    x %= 10
+if x < 0:
+    x %= -10
+if x > 5:
+    print(x, "and is greater than 5")
+if x == 0:
+    print(x, "and is zero")
+if x < 6 and x != 0:
+    print(x, "and is less than 6 and not zero")
