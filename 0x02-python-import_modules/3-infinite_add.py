b52c4381afb9d11fc04856b3fa1c3583deeb7361
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-06 12:09:50 -0500

    fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) >= 2:
-        print(sum([int(number) for number in argc[1:]]))
-    else:
-        print("{:d}.format(0))
+    result = 0
+    for i in range(1, len(argv)):
+        result += int(argv[i])
+    print("{}".format(result))
