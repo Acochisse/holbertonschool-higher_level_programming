a5468395ba69886d775c0547eb18152096c86536
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-06 12:02:23 -0500

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-n = 1:
-if n == 1:
-    print("{} arguments. ".format(n - 1))
-elif n == 2:
-    print("{} argument:".format(n - 1))
-else:
-    print("{} arguments:".format(n -1))
-for i in range(1, n):
-    print("{:d}: {:s}".format(i, argv[i]))
+    from sys import argv
+    n = len(argv):
+    if n == 1:
+        print("{} arguments. ".format(n - 1))
+    elif n == 2:
+        print("{} argument:".format(n - 1))
+    else:
+        print("{} arguments:".format(n - 1))
+    for i in range(1, n):
+        print("{:d}: {:s}".format(i, argv[i]))
