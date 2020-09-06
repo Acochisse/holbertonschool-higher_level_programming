987128f5be566984e2fad27f4e3a4ff2f11618af
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-06 12:12:02 -0500

    fixed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print("{:d}".format(i))
+    import hidden_4
+    for ch in dir(hidden_4):
+        if ch[0] != "_":
+            print("{}".format(i))
