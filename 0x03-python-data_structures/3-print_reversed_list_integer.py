3a8df20c8aa4cafc5fb84b733947a3d9800999e0
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-08 12:19:01 -0500

    changed if statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     if my_list is None:
         return None
-    else:
-        for x in range(len(my_list)):
-            print("{:d}".format(my_list[x]))
+    for x in range(len(my_list)):
+        print("{:d}".format(my_list[x]))
