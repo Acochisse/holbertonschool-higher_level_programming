0f28315e131ea7235eabcb0aab1baddf11864682
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-08 12:29:50 -0500

    added

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy = my_list
-    if idx < 0 or idx > my_list[idx]:
+    if idx < 0 or idx >= my_list[idx]:
         return copy
     else:
         my_list = list(my_list)
