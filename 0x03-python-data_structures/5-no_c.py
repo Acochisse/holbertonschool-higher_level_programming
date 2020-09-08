a44cbffbd93af17556db33717ce5e9f8d38d03c3
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-08 12:34:32 -0500

    updated if statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     stralt = ""
     for i in my_string:
-        if i != 'c' and i != 'C':
+        if my_string[i] != 'c' and my_string[i] != 'C':
             stralt += i
     return stralt
