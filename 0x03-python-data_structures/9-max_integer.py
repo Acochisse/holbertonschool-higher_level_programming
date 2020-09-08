26173e094edef374c6ebe67ed71ac2c9fed15526
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-08 12:38:19 -0500

    removed else

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 def max_integer(my_list=[]):
     alt = my_list
-    if len(alt) == 0:
-        return None
-    else:
-        alt.sort()
-        return (alt[-1])
+    if alt is None:
+        return None:
+    alt.sort()
+    return (alt[-1])
