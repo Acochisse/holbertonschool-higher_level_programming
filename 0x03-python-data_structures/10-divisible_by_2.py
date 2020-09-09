a6a14b38c8337c44bf02c3a8b49ce2a2fd1b5eb0
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-09 10:08:58 -0500

    changed if statement to ret[i]

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     ret = list(my_list)
-    for i in (len(my_list):
+    for i in my_list:
         if (ret[i] % 2 == 0):
             ret[i] = True
         else:
