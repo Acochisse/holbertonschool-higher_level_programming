6c9b74d572d970de834ce6de91de2664f11f0d99
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-10 13:16:16 -0500

    created and tested

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None:
+    hi = 0
+    key = None
+    for x, y in a_dictionary.items():
+        if y > highest:
+            hi = y
+            key = x
+    return key
