598db5fbb4522f3830706dd3b647cb5da1dde820
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-10 13:47:26 -0500

    created and tested ( line three is wrong)

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    Rm = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rm = roman_string
+    list = [Rm[i[0]] for i in zip(rm, rm[1:] + rm[-1])]
+    return (sum(list))
