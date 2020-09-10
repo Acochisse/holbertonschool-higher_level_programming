ea326886f3fade870086e6b7eb8f4c3d1ff032d9
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-10 14:00:47 -0500

    fixed backwards tracking values

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,5 +4,6 @@ def roman_to_int(roman_string):
         return 0
     Rm = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     rm = roman_string
-    list = [Rm[i[0]] for i in zip(rm, rm[1:] + rm[-1])]
+    list = [Rm[i[0]] if Rm[i[0]] >= Rm[i[1]] else (-1 * Rm[i[0]])
+            for i in zip(rm, rm[1:] + rm[-1])]
     return (sum(list))
