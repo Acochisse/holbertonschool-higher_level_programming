4442b0e4ac9127f23ee1cc5d290fc590dd11818f
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-29 10:06:39 -0500

    updated formatting

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,29 +5,31 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer function"""
+
     def test_normal_use(self):
         self.assertEqual(max_integer(list=[1, 2, 3, 4]), 4)
 
-    def no_imput(self):
+    def test_no_imput(self):
         self.assertFalse(max_integer())
 
-    def empty_list(self):
+    def test_empty_list(self):
         self.assertFalse(max_integer([]))
 
-    def negative_list(self):
+    def test_negative_list(self):
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-    def duplicates(self):
+    def test_duplicates(self):
         self.assertEqual(max_integer([1, 2, 4, 4]), 4)
 
-    def one_element(self):
+    def test_one_element(self):
         self.assertEqual(max_integer([1]), 1)
 
-    def long_value(self):
+    def test_long_value(self):
         self.assertEqual(max_integer([1000, 10000, 100000,\
                                             1000000]), 1000000)
 
-    def no_int_in_list(self):
+    def test_no_int_in_list(self):
         with self.assertRaises(TypeError):
             max_integer(["i", 2, 3, 4])
 
