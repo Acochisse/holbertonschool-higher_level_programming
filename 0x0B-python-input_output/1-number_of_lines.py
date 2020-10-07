392c444afef6008519918922baa5fc8193ef8e2b
1819@holbertonschool.com <1819@holbertonschool.com>
2020-10-07 17:10:01 -0500

    changing to be just filename inside open statement

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -7,7 +7,7 @@ module that returns the number of lines in a text file
 def number_of_lines(filename=""):
     """reads and then returns number of lines"""
 
-    with open("my_file_0.txt", 'r') as f:
+    with open(filename, 'r') as f:
         lines = 0
         for line in f:
             lines += 1
