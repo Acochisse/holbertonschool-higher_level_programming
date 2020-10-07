7925f8533315e0367f121b1377db932e82585f5d
1819@holbertonschool.com <1819@holbertonschool.com>
2020-10-07 17:11:43 -0500

    changed encoding and filename

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,7 +7,7 @@ module that returns the number of lines in a text file
 def read_lines(filename="", nb_lines=0):
     """reads the file and returns the specified number of lines"""
 
-    with open("my_file_0.txt", encoding='UTF-8') as f:
+    with open(filename, 'r') as f:
         if nb_lines <= 0:
             print(f.read(), end="")
         for i in range(nb_lines):
