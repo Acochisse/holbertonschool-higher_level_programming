4ce010107a5e251373f56c6a5e6544bf331ecbcb
1819@holbertonschool.com <1819@holbertonschool.com>
2020-12-18 12:18:53 -0600

    fixed spelling errors

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script
-curl -sX "email=hr@holbertonschool.com&subject=I will always be here for PLD" "s1"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
