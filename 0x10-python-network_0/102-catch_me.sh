ddb42a124a8f4cbf8960308bec531f3993510be4
1819@holbertonschool.com <1819@holbertonschool.com>
2020-12-21 11:51:50 -0600

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script
-curl -sLX 'user_id=89' -H 'Origin: HolbertonSchool' 0.0.0.0:5000/catch_me
+# a Bash script that makes a request to 0.0.0.0:5000/catch_me that
+curl -sLX PUT -d 'user_id=98' -H 'Origin: HolbertonSchool' 0.0.0.0:5000/catch_me
