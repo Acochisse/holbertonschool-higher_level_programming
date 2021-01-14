e069407c999f4e2cad0b8cb48eba38a3e7189525
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-14 12:17:55 -0600

    fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Script that creates a new user with all permissions on server
 
-CREATE USER IF NOT EXISTS "user_0d_1""@""localhost";
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
