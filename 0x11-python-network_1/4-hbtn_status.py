c7e890bf14a82e299bdc5ac7f739f979db9a609d
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-14 11:23:50 -0600

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,9 +5,9 @@ script
 import requests
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     info = r.text
+    print("Body response:")
     print("\t- type: {}".format(type(info)))
     print("\t- content: {}".format(info))
-    
