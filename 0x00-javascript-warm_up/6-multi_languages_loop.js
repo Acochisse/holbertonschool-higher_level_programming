e46cbe26d39fca26f8670fafad0ae3abf85ea066
1819@holbertonschool.com <1819@holbertonschool.com>
2022-01-06 13:32:47 -0600

    JavaScript

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const line = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let step = 0; step < line.length; step++) {
   console.log(line[step]);
 }
