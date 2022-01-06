29e3f8c7e212d5221bbe5e2405bc493027b80e05
1819@holbertonschool.com <1819@holbertonschool.com>
2022-01-06 08:02:22 -0600

    adding task 0 and 1

---
+++ 0x00-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// script that prints "Javascript is amazing
+const txt = 'Javascript is amazing';
+console.log(txt);
---
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that displays 3 lines about multi languages
+const c = "C is fun";
+const python = "Python is cool";
+const java = "Javascript is amazing";
+console.log(c);
+console.log(python);
+console.log(java);
