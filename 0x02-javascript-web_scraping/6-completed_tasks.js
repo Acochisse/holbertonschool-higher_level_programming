bdf6c13ef54ccb65c6d03bf5ee291a01aad23ddf
1819@holbertonschool.com <1819@holbertonschool.com>
2022-01-11 13:42:30 -0600

    rewrite

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
 
 const url = process.argv[2];
-const filePath = process.argv[3];
 const request = require('request');
-const fs = require('fs')
+const myJSON = {};
 
-request(url, function (errorId, response, body){
-    if (errorId) {
-        console.log(errorId);
-    } else {
-        fs.writeFile(filePath, body, 'utf-8', function(errorId){
-            if (errorId) {
-                console.log(errorId);
-            }
-        });
+request(url, function (errorId, response, body) {
+  if (errorId) {
+    console.log(errorId);
+  } else {
+    for (const user of JSON.parse(body)) {
+      if (user.completed === true) {
+        if (!(user.userId in myJSON)) {
+          myJSON[user.userId] = 1;
+        } else {
+          myJSON[user.userId] = myJSON[user.userId] + 1;
+        }
+      }
     }
+  }
+  console.log(myJSON);
 });
